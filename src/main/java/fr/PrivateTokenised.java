bba2fc7a59dba995c9b7d8c6754109f8dbdaa54c
Paul Bourgeois <paul.bourgeois@sap.com>
2018-08-09 16:06:21 +0200

    first commit

---
+++ src/main/java/fr/PrivateTokenised.java
@@ -0,0 +1,7 @@
+package fr;
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public abstract class PrivateTokenised extends ListenerAdapter {
+    public static String token = "Insert-bot-token-here";
+}
